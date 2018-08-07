2a68ab7b78429591063bf0dfb543c1ac35e528a5
qq410466262 <410466262@qq.com>
2018-08-07 09:22:18 +0800

    修改项目进度接口

--- Plough-server/Home/Lib/Action/ProjectProcessAction.class.php
+++ Plough-server/Home/Lib/Action/ProjectProcessAction.class.php
@@ -1,12 +1,6 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: dhj
- * Date: 2018/6/6
- * Time: 15:52
- * Description: 项目进度
- */
+
 class ProjectProcessAction extends Action
 {
     /**
@@ -31,19 +25,19 @@ class ProjectProcessAction extends Action
         $where ['id'] = $data['id'];
         if($Process->where($where)->save($data)){
             $projects = M('projects');
-            $latest = $Process->where('relatedId='.$data['relatedId'])->order('id desc')->limit(1)->select();
-            $tempData = array(
-                'risk' => $latest[0]['risk'],
-                'responseMeasures' => $latest[0]['responseMeasures'],
-                'currentProgress' => $latest[0]['currentProgress'],
-            );
-            $result = $projects->where('id='.$data['relatedId'])->save($tempData);
-            if ($result){
+//            $latest = $Process->where('relatedId='.$data['relatedId'])->order('id desc')->limit(1)->select();
+//            $tempData = array(
+//                'risk' => $latest[0]['risk'],
+//                'responseMeasures' => $latest[0]['responseMeasures'],
+//                'currentProgress' => $latest[0]['currentProgress'],
+//            );
+//            $result = $projects->where('id='.$data['relatedId'])->save($tempData);
+//            if ($result){
                 $projectName = $projects->where('id=' . $data['relatedId'])->field('projectName')->select();
                 $logs = M('logs_info');
                 $logsData ['logDetail'] = date('Y-m-d H:i:s') . ':' . session('userName') . '修改项目(' . $projectName[0]['projectName'] . ")进展信息";
                 $logs->add($logsData);
-            }
+//            }
             $this->ajaxReturn(array(),'修改成功',1);
         }else{
             $this->ajaxReturn(array(),'修改失败',0);
@@ -55,13 +49,13 @@ class ProjectProcessAction extends Action
      */
     public function getProcess($id){
         $process = M('project_process');
-        $result = $process->where('relatedId='.$id)->order('updateTime desc')->find();
-        if(!$result || strtotime('last sunday') - strtotime(substr($result[updateTime], 0, 10))> 0){
-            $row['relatedId'] = $id;
-            $row['process'] = 0;
-            $row['risk'] = '高';
-            $process->add($row);
-        }
+//        $result = $process->where('relatedId='.$id)->order('updateTime desc')->find();
+//        if(!$result || strtotime('last sunday') - strtotime(substr($result[updateTime], 0, 10))> 0){
+//            $row['relatedId'] = $id;
+//            $row['process'] = 0;
+//            $row['risk'] = '高';
+//            $process->add($row);
+//        }
         $data = $process->where('relatedId='.$id)->order('updateTime desc')->select();
         if($data !== false){
             $this->ajaxReturn($data,'获取成功', 1);
@@ -123,6 +117,33 @@ class ProjectProcessAction extends Action
             $this->ajaxReturn(array(),'删除成功', 1);
         }else{
             $this->ajaxReturn(array(),'删除失败', 0);
+        }
+    }
+
+    public function autoAdd(){
+        $currentTime['updateTime'] = date('Y-m-d H:i:s');
+//        $start = date('Y-m-d H:i:s', strtotime('last monday'));
+//        $end = date('Y-m-d H:i:s', strtotime('this tuesday') + 7 * 24 * 60 * 60);
+        $start = date('Y-m-d',(time()-((date('w')==0?7:date('w'))-1)*24*3600));//本周一
+        $end = date('Y-m-d',(time()+(7-(date('w')==0?7:date('w'))+1)*24*3600));//下周一
+        $projects = M('projects');
+        $result = $projects->where('risk!=""')->field('id,risk')->order('id asc')->select();
+        $process = M('project_process');
+        foreach($result as $key => $value)
+        {
+            $tempData['relatedId'] = $value['id'];
+            $tempData['risk'] = $value['risk'];
+            $data = array_merge($tempData,$currentTime);
+            //如果本周已经存在某项目，则不添加
+            $where['relatedId'] = $value['id'];
+            $where['updateTime'] = array('between',array($start,$end));
+            if(!$process->where($where)->select()) {
+                $process->add($data);
+                $projectName = $projects->where('id=' . $value['id'])->field('projectName')->select();
+                $logs = M('logs_info');
+                $logsData ['logDetail'] = date('Y-m-d H:i:s') . ':'  . '系统自动添加项目(' . $projectName[0]['projectName'] . ")进展信息";
+                $logs->add($logsData);
+            }
         }
     }
 }
